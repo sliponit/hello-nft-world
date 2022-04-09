21e32abfad1dda60dfa7e6b67683dbc4cac74ac8
sliponit <sliponit@pm.me>
2022-04-09 09:10:01 +0200

    before new contract

--- app/src/App.js
+++ app/src/App.js
@@ -1,44 +1,37 @@
 import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import openseaLogo from './assets/opensea-logo.svg';
 import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
-import ABI from './utils/abi.json';
+import { ABI, CONTRACT_ADDRESS, OPENSEA_LINK } from './utils/contract';
 
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
-// I moved the contract address to the top for easy access.
-const CONTRACT_ADDRESS = process.env.REACT_CONTRACT_ADDRESS
-
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [total, setTotal] = useState(0);
+  
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
 
-    const [currentAccount, setCurrentAccount] = useState("");
-    
-    const checkIfWalletIsConnected = async () => {
-      const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
 
-      if (!ethereum) {
-          console.log("Make sure you have metamask!");
-          return;
-      } else {
-          console.log("We have the ethereum object", ethereum);
-      }
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
 
-      const accounts = await ethereum.request({ method: 'eth_accounts' });
-
-      if (accounts.length !== 0) {
-          const account = accounts[0];
-          console.log("Found an authorized account:", account);
-					setCurrentAccount(account)
-          
-          // Setup listener! This is for the case where a user comes to our site
-          // and ALREADY had their wallet connected + authorized.
-          setupEventListener()
-      } else {
-          console.log("No authorized account found")
-      }
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener()
+    } else {
+      console.log("No authorized account found");
+    }
   }
 
   const connectWallet = async () => {
@@ -56,7 +49,7 @@ const App = () => {
       setCurrentAccount(accounts[0]);
 
       // Setup listener! This is for the case where a user comes to our site
-      // and connected their wallet for the first time.
+      // and connected their wallet for the first time. TODO?? double with state
       setupEventListener() 
     } catch (error) {
       console.log(error)
@@ -84,7 +77,10 @@ const App = () => {
         });
 
         console.log("Setup event listener!")
-
+        // TODO?? move
+        const total = await connectedContract.getTotalNFTsMintedSoFar();
+        setTotal(total.toNumber());
+        console.log("Retrieved total count...", total.toNumber());
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -123,6 +119,7 @@ const App = () => {
     checkIfWalletIsConnected();
   }, [])
 
+  // Render Methods
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
@@ -130,9 +127,14 @@ const App = () => {
   );
 
   const renderMintUI = () => (
-    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
-      Mint NFT
-    </button>
+    <div>
+      <p className="sub-text">
+        Minted { total } / { TOTAL_MINT_COUNT } 
+      </p>
+      <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+        Mint NFT
+      </button>
+    </div>
   )
 
   return (
@@ -146,13 +148,13 @@ const App = () => {
           {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
         </div>
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <img alt="Opensea Logo" className="opensea-logo" src={openseaLogo} />
           <a
             className="footer-text"
-            href={TWITTER_LINK}
+            href={OPENSEA_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >View collection</a>
         </div>
       </div>
     </div>
