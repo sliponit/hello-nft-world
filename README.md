174fa88a0d17ee624fbf2cb4c1f647d17bc1f563
sliponit <sliponit@pm.me>
2022-04-14 22:45:59 +0200

    typos

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Create a fun experience to learn not to buy/sell NFTs too fast, i.e. before a go
 - A NFT collection of 40 Sleepy Cartoon Characters on the Ethereum blockchain (Rinkeby testnet)
 - Each character has a unique sleeping duration between 9 and 48 hours.
 - The sleeping duration depends on the traits of the character, e.g. a smiling characters needs less sleep than a sad one. Rare characters have a red element and the longest sleeping need!
-- This sleeping duration needs to be respected between buying an NFT and listing it for sale.
+- This sleeping duration needs to be respected between buying a NFT and listing it for sale.
 - Otherwise the character goes in hibernation between 9 and 48 days!
 - When a characters is asleep, it appears with a grey background. When it hibernates, it goes black!
 - One mint per wallet. The wallet address appears as a watermark on the NFT image.
@@ -23,10 +23,10 @@ Create a fun experience to learn not to buy/sell NFTs too fast, i.e. before a go
 - scripts 
   - init.js to initialize small DB on cloudflare KV
   - pinata.js to create ipfs files
-  - cron.js backend script updating data every minute (should be moved in api for consistency)
+  - cron.js backend script updating data every minute (should be moved in api for consistency, see https://developers.cloudflare.com/workers/examples/cron-trigger/)
 
 ## Known limitation
-- Offer acceptation in WETH not taken into account
+- Offer acceptation in WETH and transfers not taken into account
 
 ## Credits
 - https://freesvg.org/cartoon-mouth-and-eyes for art elements
--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
 import { ABI, CONTRACT_ADDRESS, OPENSEA_LINK } from './utils/contract';
 
-const TOTAL_MINT_COUNT = 50;
+const TOTAL_MINT_COUNT = 40;
 const PRICE = '0.1';
 const WORKER_URL = 'https://nft-api.sliponit9471.workers.dev'
 
@@ -188,10 +188,10 @@ const App = () => {
     <div className="body-container">
       <ul>
         <li>Vision: Create a fun experience to learn not to buy/sell NFTs too fast, i.e. before a good night sleep</li>
-        <li> A NFT collection of 40 Sleepy Cartoon Characters on the Ethereum blockchain (Rinkeby testnet)</li>
+        <li>A NFT collection of 40 Sleepy Cartoon Characters on the Ethereum blockchain (Rinkeby testnet)</li>
         <li>Each character has a unique sleeping duration between 9 and 48 hours.</li>
         <li>The sleeping duration depends on the traits of the character, e.g. a smiling characters needs less sleep than a sad one. Rare characters have a red element and the longest sleeping need!</li>
-        <li>This sleeping duration needs to be respected between buying an NFT and listing it for sale.</li>
+        <li>This sleeping duration needs to be respected between buying a NFT and listing it for sale.</li>
         <li>Otherwise the character goes in hibernation between 9 and 48 days!</li>
         <li>When a characters is asleep, it appears with a grey background. When it hibernates, it goes black!</li>
         <li>One mint (0.1 ETH) per wallet. The wallet address appears as a watermark on the NFT image.</li>
